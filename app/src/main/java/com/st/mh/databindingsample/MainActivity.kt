fdea0269ffcf1eedb4748dbf33780a5199b4c218
kndol2 <kndol2@naver.com>
2018-11-11 23:03:22 +0900

    add databinding setting

--- app/src/main/java/com/st/mh/databindingsample/MainActivity.kt
+++ app/src/main/java/com/st/mh/databindingsample/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.st.mh.databindingsample
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.st.mh.databindingsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.activity = this
+    }
+
+    fun onOk(view: View) {
+        binding.textTitle.text = "OK"
+    }
+
+    fun onCancel(view: View) {
+        binding.textTitle.text = "Cancel"
     }
 }
