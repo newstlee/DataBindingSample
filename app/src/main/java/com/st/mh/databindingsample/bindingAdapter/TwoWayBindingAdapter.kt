ba00a455cb698a6d24464a88833bcf43daeb0be3
seoktaelee <stlee@stunitas.com>
2018-11-12 22:32:47 +0900

    add custom 2-way databinding

---
+++ app/src/main/java/com/st/mh/databindingsample/bindingAdapter/TwoWayBindingAdapter.kt
@@ -0,0 +1,41 @@
+package com.st.mh.databindingsample.bindingAdapter
+
+import android.databinding.BindingAdapter
+import android.databinding.InverseBindingAdapter
+import android.databinding.InverseBindingListener
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
+
+class TwoWayBindingAdapter {
+
+    companion object {
+        @BindingAdapter("text")
+        @JvmStatic fun setTextString(view: TextView, text: String?) {
+            val old = view.text.toString()
+            if (old != text) {
+                view.text = text
+            }
+        }
+
+        @InverseBindingAdapter(attribute = "text", event = "textAttrChanged")
+        @JvmStatic fun getTextString(view: TextView): String {
+            return view.text.toString()
+        }
+
+        @BindingAdapter("textAttrChanged")
+        @JvmStatic fun setTextWatcher(view: TextView, textAttrChanged: InverseBindingListener?) {
+            view.addTextChangedListener(object : TextWatcher{
+                override fun afterTextChanged(s: Editable?) {
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    textAttrChanged?.onChange()
+                }
+            })
+        }
+    }
+}
--- app/src/main/java/com/st/mh/databindingsample/model/TwoWayModel.kt
+++ app/src/main/java/com/st/mh/databindingsample/model/TwoWayModel.kt
@@ -2,6 +2,8 @@ package com.st.mh.databindingsample.model
 
 import android.databinding.ObservableField
 
+
 class TwoWayModel {
-    val contents = ObservableField<String>()
+    val contents: ObservableField<String> = ObservableField()
+    val contents2: ObservableField<String> = ObservableField("custom 2 way")
 }
